af5c1b1b43f2730cc4856a9879964ad8e45a4c2c
weiyongzhi <weiyongzhi@300.cn>
2019-01-03 09:59:01 +0800

    新增文件

--- packagingComponents/gulpfile.js
+++ packagingComponents/gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 gulp.task('jsmin',function(){
-    gulp.src(["/dir/"+equipment+"/c_"+folderName+"-01001/*.js","!/dir/"+equipment+"/c_"+folderName+"-01001/*min.js"])
+    gulp.src(["D:/dir/"+equipment+"/c_"+folderName+"-01001/*.js","!D:/dir/"+equipment+"/c_"+folderName+"-01001/*min.js"])
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest("/dir/"+equipment+"/c_"+folderName+"-01001/"))
+    .pipe(gulp.dest("D:/dir/"+equipment+"/c_"+folderName+"-01001/"))
 });
