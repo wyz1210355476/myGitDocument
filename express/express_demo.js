42448fe947758b988420606ab0f59381504890ea
weiyongzhi <weiyongzhi@300.cn>
2019-05-10 10:00:01 +0800

    新增

--- express/express_demo.js
+++ express/express_demo.js
@@ -35,7 +35,7 @@ app.post('/file_upload', function (req, res) {
    
 })
  
-var server = app.listen(8081, function () {
+var server = app.listen(8080, function () {
  
   var host = server.address().address
   var port = server.address().port
--- express/test.js
+++ express/test.js
@@ -1,14 +1,31 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '123456',
-  database : 'mysql'
-});
+var mysql  = require('mysql');  
+ 
+var connection = mysql.createConnection({     
+  host     : 'localhost',       
+  user     : 'root',              
+  password : '123456',       
+  port: '3306',                   
+  database: 'nodetest' 
+}); 
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+var $sql = "CREATE TABLE runoob_tbl( "
+"runoob_id INT NOT NULL AUTO_INCREMENT, "
+"runoob_title VARCHAR(100) NOT NULL, "
+"runoob_author VARCHAR(40) NOT NULL, "
+"submission_date DATE, "
+"PRIMARY KEY ( runoob_id ))ENGINE=InnoDB DEFAULT CHARSET=utf8; ";
+// var modSqlParams = ['菜鸟移动站', 'https://m.runoob.com',6];
+//改
+connection.query($sql,function (err, result) {
+   if(err){
+         console.log('[UPDATE ERROR] - ',err.message);
+         return;
+   }        
+  console.log('--------------------------UPDATE----------------------------');
+  console.log('UPDATE affectedRows',"www");
+  console.log('-----------------------------------------------------------------\n\n');
 });
+ 
+connection.end();
