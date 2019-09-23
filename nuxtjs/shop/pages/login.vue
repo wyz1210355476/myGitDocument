7dbdf64204a074ed745749711ce31d6ea808c6f5
魏永志 <weiruoyu@example>
2019-09-23 23:00:12 +0800

    nuxtjs

---
+++ nuxtjs/shop/pages/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="账号:" prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+        </el-form-item>
+        <el-form-item label="密码:" prop="password">
+            <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+    data(){
+        return{
+            ruleForm:{
+                account: '',
+                password: ''
+            }
+        }
+    },
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ nuxtjs/shop/pages/register.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+        <el-form-item label="手机号:" prop="account">
+            <el-input v-model="ruleForm.account"></el-input>
+        </el-form-item>
+        <el-form-item label="密码:" prop="password">
+            <el-input v-model="ruleForm.password"></el-input>
+        </el-form-item>
+        <el-form-item label="" prop="password">
+            <el-button type="primary" @click="register">注册</el-button>
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+    data(){
+        return{
+            ruleForm:{
+                account: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        register(){
+            this.$message({
+                message: '恭喜你，这是一条成功消息',
+                type: 'success'
+            });
+        }
+    },
+    components: {
+        Logo
+    }
+}
+</script>
+
+<style>
+.container {
+  /* margin: 0 auto; */
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
